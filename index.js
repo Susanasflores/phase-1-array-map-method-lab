eb90b52a105622465f07783d9c40e321b58f2a3d
Susana Solis Flores <Suzy.flores@icloud.com>
2023-09-23 02:05:54 -0400

    Array map method

--- index.js
+++ index.js
@@ -12,5 +12,18 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+    const modifiedTutorials = tutorials.map((tutorial) => {
+      const words = tutorial.split(" ");
+      const capitalizedWords = words.map((word) => {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      });
+      return capitalizedWords.join(" ");
+    });
+  
+    return modifiedTutorials;
+  };
+  
+  console.log(titleCased());
+
+  
+
